0a85f15f065ace0bcd8524451b358d4079c4d66a
Oscar Patiño <gposcarr18@gmail.com>
2019-09-03 08:01:30 -0500

    Algunos problemas actualizados

--- 2019 RPC_07 Julio-13/C/C.cpp
+++ 2019 RPC_07 Julio-13/C/C.cpp
@@ -4,41 +4,62 @@
 using namespace std;
 
 class Graph {
-	int v; //nodos
-	list<int> *adj;
+    int v; //nodos
+    list<int> *adj;
 public:
-	Graph(int v);
-	void addEdge(int v1, int v2);
-	void topological(int)
+    Graph(int v);
+    void addEdge(int v1, int v2);
+    void topological(int, bool visited[], stack<int> &stack);
+    void topologicalSort();
 };
 
 Graph::Graph(int v) {
-	this->v = v;
-	adj = new list<int>[v];
+    this->v = v;
+    adj = new list<int>[v];
 }
 
 void Graph::addEdge(int v1, int v2) {
-	adj[v1].push_back(v2);
+    adj[v1].push_back(v2);
 }
 
-bool topologico() {
-	return true;
+void Graph::topological(int v, bool visited[], stack<int> &stack) {
+    visited[v] = true;
+    for_each(begin(*adj), end(*adj), [this, visited, &stack](int i){if (!visited[i]) topological(i, visited, stack);});
+    stack.push(v);
+}
+
+void Graph::topologicalSort() {
+    stack<int> stack;
+    bool *visited = new bool[v];
+
+    for (int i = 0; i < v; i++) {
+        visited[i] = false;
+    }    
+    //for_each(begin(visited), end(visited), [](int& i){i = false;});
+
+    //for_each(begin(visited), end(visited), [visited](int i){})
+    for (int i = 0; i < v; i++) {
+        if (visited[i] == false) {
+            topological(i, visited, stack); 
+        }
+    }
 }
 
 int main() {
-	int no;	
-	int ed;
+    int no; 
+    int ed;
+    int scr, dest, weight;
+    cin >> no >> ed;
 
-	cin >> no >> ed;
+    Graph g(no);
+    while (ed--) {
+        int src, dest, weight;
+        
+    }
+    //for_each(begin(edges), end(edges), [](pair<int, pair<int, int>> &i){cout << i.top() << endl; i.pop()});
+    //for_each(begin(edges), end(edges), [](pair<int, pair<int, int>> i){cout << i.first << endl;});
 
-	Graph g(no);
-	while (ed--) {
-		
-	}
-	//for_each(begin(edges), end(edges), [](pair<int, pair<int, int>> &i){cout << i.top() << endl; i.pop()});
-	//for_each(begin(edges), end(edges), [](pair<int, pair<int, int>> i){cout << i.first << endl;});
-
-	//Dijstra
-	vector<int> nodes;
+    //Dijstra
+    vector<int> nodes;
 
 }
